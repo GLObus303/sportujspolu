8a909b326f8cd8ad0c3a65ccf79caf0030d475da
johanarybarova <johanarybarova@gmail.com>
2023-03-31 08:29:17 +0200

    Connect to events GET api

--- src/components/Events.tsx
+++ src/components/Events.tsx
@@ -1,44 +1,44 @@
-export const mockEvents = [
-  {
-    id: 1,
-    name: "Vendy",
-    sport: "Basketball",
-  },
-  {
-    id: 2,
-    name: "Lukas",
-    sport: "Football",
-  },
-  {
-    id: 3,
-    name: "Honza",
-    sport: "Tennis",
-  },
-  {
-    id: 4,
-    name: "Vendy",
-    sport: "Box",
-  },
-  {
-    id: 5,
-    name: "Lukas",
-    sport: "Skydiving",
-  },
-  {
-    id: 6,
-    name: "Johana",
-    sport: "Jogging",
-  },
-];
+import { useState, useEffect } from "react";
 
-export const Events: React.FC = () => (
-  <>
-    {mockEvents.map(({ id, sport, name }) => (
-      <article key={id}>
-        <p>{sport}</p>
-        <p>od: {name}</p>
-        <button>Otevřít</button>
-      </article>
-    ))}
-  </>
-);
+interface Event {
+  id: number;
+  sport: string;
+  name: string;
+}
+
+const fetchEventsData = async () => {
+  const response = await fetch("https://sportujspolu-api.onrender.com/events");
+  if (!response.ok) {
+    throw new Error("Failed to fetch data");
+  }
+  const events = await response.json();
+  return events as Event[];
+};
+
+export const Events: React.FC = () => {
+  const [events, setEvents] = useState<Event[]>([]);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const events = await fetchEventsData();
+        setEvents(events);
+        console.log("events:", events);
+      } catch (error) {
+        console.error(error);
+      }
+    })();
+  }, []);
+
+  return (
+    <>
+      {events.map(({ id, sport, name }) => (
+        <article key={id}>
+          <p>{sport}</p>
+          <p>od: {name}</p>
+          <button>Otevřít</button>
+        </article>
+      ))}
+    </>
+  );
+};
