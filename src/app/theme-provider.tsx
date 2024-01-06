7847644c1d415de0d97a37c9f00bcdd7965ebeda
Johana Rybarova <johanarybarova@gmail.com>
2024-01-06 11:37:35 +0100

    Fix

--- src/app/theme-provider.tsx
+++ src/app/theme-provider.tsx
@@ -3,6 +3,6 @@ import * as React from 'react';
 import { ThemeProvider as NextThemesProvider } from 'next-themes';
 import { ThemeProviderProps } from 'next-themes/dist/types';
 
-export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
-}
+export const ThemeProvider = ({ children, ...props }: ThemeProviderProps) => (
+  <NextThemesProvider {...props}>{children}</NextThemesProvider>
+);
