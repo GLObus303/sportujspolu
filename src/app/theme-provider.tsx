ef0f090caa402df0d319b6a241399bafcc9b43f0
Johana Rybarova <johanarybarova@gmail.com>
2024-02-25 23:39:02 +0100

    Fix

--- src/app/theme-provider.tsx
+++ src/app/theme-provider.tsx
@@ -1,8 +1,19 @@
 'use client';
-import * as React from 'react';
+
 import { ThemeProvider as NextThemesProvider } from 'next-themes';
 import { ThemeProviderProps } from 'next-themes/dist/types';
+import { useState, useEffect } from 'react';
 
-export const ThemeProvider = ({ children, ...props }: ThemeProviderProps) => (
-  <NextThemesProvider {...props}>{children}</NextThemesProvider>
-);
+export const ThemeProvider = ({ children, ...props }: ThemeProviderProps) => {
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) {
+    return null;
+  }
+
+  return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
+};
