8272de9b4ccf594152b513d19ac28960f0700446
Johana Rybarova <johanarybarova@gmail.com>
2024-05-04 09:29:59 +0200

    Format date inside server component

--- src/components/Events/EventCard.tsx
+++ src/components/Events/EventCard.tsx
@@ -2,13 +2,11 @@
 
 import Link from 'next/link';
 import Image from 'next/image';
-// import { useMemo } from 'react';
 
 import { StarRating } from '../StarRating';
 import { HeartButton } from '../HeartButton';
 import { Event } from '../../types/Event';
 import { Routes } from '../../utils/constants';
-// import { formatDateTime } from '../../utils/dateUtils';
 import { getImagePath } from '../../utils/functions';
 
 const rating = 3.5;
@@ -16,60 +14,57 @@ const rating = 3.5;
 type EventCardProps = {
   event: Event;
   index: number;
+  formattedDate: string;
 };
 
 export const EventCard: React.FC<EventCardProps> = ({
-  event: { id, name, sport, date, location, price, description, level },
+  event: { id, name, sport, location, price, description, level },
   index,
-}) => {
-  // const formattedDate = useMemo(() => formatDateTime(date), [date]);
-  const formattedDate = date;
-
-  return (
-    <article className="relative rounded-md bg-card shadow-md">
-      <Link href={`${Routes.EVENT}/${id}`}>
-        <div style={{ aspectRatio: '1/1' }} className="rounded-md bg-card">
-          <div className="relative h-1/3 w-full overflow-hidden">
-            <Image
-              alt=""
-              src={getImagePath(id, sport)}
-              className="rounded-tl-md rounded-tr-md object-cover"
-              sizes="auto"
-              priority={index < 5}
-              fill
-            />
+  formattedDate,
+}) => (
+  <article className="relative rounded-md bg-card shadow-md">
+    <Link href={`${Routes.EVENT}/${id}`}>
+      <div style={{ aspectRatio: '1/1' }} className="rounded-md bg-card">
+        <div className="relative h-1/3 w-full overflow-hidden">
+          <Image
+            alt=""
+            src={getImagePath(id, sport)}
+            className="rounded-tl-md rounded-tr-md object-cover"
+            sizes="auto"
+            priority={index < 5}
+            fill
+          />
+        </div>
+        <div className="flex h-2/3 flex-col justify-between p-5">
+          <p className="text-s flex flex-row justify-between font-light">
+            <span className="w-1/2 truncate">{location}</span>{' '}
+            <time className="truncate">{formattedDate}</time>
+          </p>
+          <p className="line-clamp-3 overflow-hidden overflow-ellipsis">
+            <span className="font-medium">{name}:</span>{' '}
+            <span className="font-light">{description}</span>
+          </p>
+          <div className="flex items-center">
+            <span className="pr-1 font-light text-dark-gray dark:text-accent">
+              userName
+            </span>
+            <StarRating rating={rating} />
           </div>
-          <div className="flex h-2/3 flex-col justify-between p-5">
-            <p className="text-s flex flex-row justify-between font-light">
-              <span className="w-1/2 truncate">{location}</span>{' '}
-              <time className="truncate">{formattedDate}</time>
-            </p>
-            <p className="line-clamp-3 overflow-hidden overflow-ellipsis">
-              <span className="font-medium">{name}:</span>{' '}
-              <span className="font-light">{description}</span>
-            </p>
-            <div className="flex items-center">
-              <span className="pr-1 font-light text-dark-gray dark:text-accent">
-                userName
+          <div className="flex items-center justify-between">
+            <p>
+              <span className="font-medium">{sport}</span>
+              <span className="font-light">
+                {' | '}
+                {level === 'Any' ? 'All Levels' : level}
               </span>
-              <StarRating rating={rating} />
-            </div>
-            <div className="flex items-center justify-between">
-              <p>
-                <span className="font-medium">{sport}</span>
-                <span className="font-light">
-                  {' | '}
-                  {level === 'Any' ? 'All Levels' : level}
-                </span>
-              </p>
-              <p className="font-medium">
-                {price === 0 ? 'ZDARMA' : `${price} Kč`}
-              </p>
-            </div>
+            </p>
+            <p className="font-medium">
+              {price === 0 ? 'ZDARMA' : `${price} Kč`}
+            </p>
           </div>
         </div>
-      </Link>
-      <HeartButton className="absolute right-3 top-3" />
-    </article>
-  );
-};
+      </div>
+    </Link>
+    <HeartButton className="absolute right-3 top-3" />
+  </article>
+);
--- src/components/Events/index.tsx
+++ src/components/Events/index.tsx
@@ -1,4 +1,5 @@
 import { Event } from '../../types/Event';
+import { formatDate } from '../../utils/dateUtils';
 import { EventCard } from './EventCard';
 
 type EventsProps = {
@@ -10,7 +11,12 @@ export const Events: React.FC<EventsProps> = ({ events }) => (
     <section className="flex justify-center">
       <div className="mt-10 grid max-w-[350px] grid-cols-1 gap-5 gap-y-5 sm:max-w-[750px] sm:grid-cols-2 sm:px-5 md:mt-14 md:px-0 lg:max-w-[1000px] lg:grid-cols-3 xl:max-w-[1440px] xl:grid-cols-4">
         {events.map((event, index) => (
-          <EventCard key={event.id} event={event} index={index} />
+          <EventCard
+            key={event.id}
+            event={event}
+            index={index}
+            formattedDate={formatDate(event.date)}
+          />
         ))}
       </div>
     </section>
