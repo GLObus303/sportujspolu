07067525f12d596c3d997acf958edd702dbb9fd4
Lukas Cizek <globus303@gmail.com>
2025-01-12 21:23:11 +0100

    Fix hotjar script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -35,18 +35,21 @@ const RootLayout: ChildrenFC = async ({ children }) => {
       style={{ colorScheme: defaultTheme }}
     >
       <head>
-        <Script id="hotjar-script" strategy="afterInteractive">
-          {`
-      (function(h,o,t,j,a,r){
-          h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-          h._hjSettings={hjid:5264674,hjsv:6};
-          a=o.getElementsByTagName('head')[0];
-          r=o.createElement('script');r.async=1;
-          r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-          a.appendChild(r);
-      })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
-    `}
-        </Script>
+        <script
+          id="hotjar-script"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(h,o,t,j,a,r){
+                  h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+                  h._hjSettings={hjid:5264674,hjsv:6};
+                  a=o.getElementsByTagName('head')[0];
+                  r=o.createElement('script');r.async=1;
+                  r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+                  a.appendChild(r);
+              })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+            `,
+          }}
+        />
         <meta name="robots" content="index, follow" />
       </head>
       <body>
