6e9d06a937cfe7d4e42e44736470088e78949fa9
Johana Rybarova <johanarybarova@gmail.com>
2025-07-24 21:23:43 +0200

    Fix

--- src/components/icons/sports/OtherSportIcon.tsx
+++ src/components/icons/sports/OtherSportIcon.tsx
@@ -16,21 +16,15 @@ export const OtherSportIcon: React.FC<SVGProps<SVGSVGElement>> = (props) => (
     />
     <path
       stroke="#000"
-      strokeLinejoin="round"
-      strokeMiterlimit="10"
       d="M33.75 38.15 20 32.35l-13.74 5.8V1.26h27.49v36.89Z"
     />
     <path
       fill="#94FFA6"
       stroke="#000"
-      strokeLinejoin="round"
-      strokeMiterlimit="10"
       d="M20 22.2a8.18 8.18 0 1 0 0-16.36 8.18 8.18 0 0 0 0 16.35Z"
     />
     <path
       stroke="#000"
-      strokeLinejoin="round"
-      strokeMiterlimit="10"
       d="M33.75 32.8 20 27 6.26 32.8M33.71 1.26h5.12V5.8h-5.12m-27.45 0h-5.1V1.26h5.1M20 10.58 21.43 12l1.79.91-.91 1.8-.32 1.99-1.99-.31-1.99.3-.32-1.98-.9-1.8 1.78-.91L20 10.58Z"
     />
   </svg>
